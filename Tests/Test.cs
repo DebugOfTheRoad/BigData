cd512ade55e2d7d975ba23863dde86ad8f040add
Davis Gallinghouse <dgalling@gmail.com>
2014-01-20 20:18:14 -0500

    Add basic testing framework

---
+++ Tests/Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public abstract class Test
+    {
+        private Result result;
+        public Result Result
+        {
+            get { return result; }
+        }
+
+        private string reason;
+        public string Reason
+        {
+            get { return reason; }
+        }
+
+        public Test()
+        {
+            result = Result.Pending;
+        }
+
+        public void Pass()
+        {
+            result = Result.Pass;
+        }
+
+        public void Fail(string message)
+        {
+            reason = message;
+            result = Result.Fail;
+        }
+    }
+
+    public enum Result
+    {
+        Pass, Fail, Pending
+    }
+}
