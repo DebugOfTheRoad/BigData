f56293c98b9cd4fc4c191d3f3506e317d9230e0d
Daniel Eshleman <dae013@bucknell.edu>
2014-01-26 15:05:59 -0500

    OCLC wrapper pulls data, xml parsing next

--- BigData/OCLC/OCLCWrapper.cs
+++ BigData/OCLC/OCLCWrapper.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using System.Xml;
+using System.Net;
+using System.Collections.Generic;
 
 namespace BigData
 {
 
     public class OCLCWrapper
     {
+        XmlTextReader reader;
+        String xml;
+        List<Publication> publications;
         public OCLCWrapper(string rssURL)
         {
-            XmlTextReader reader = new XmlTextReader(rssURL);
+            reader = new XmlTextReader(rssURL);
+            xml = new System.Net.WebClient().DownloadString(rssURL);
+        }
+
+         public List<Publication> createPublications()
+         {
+            publications = new List<Publication>();
+            return publications;            
+        }
+
+        public String getXML()
+        {
+            return xml;
         }
 
     }
--- TestBigData/TestAPI.cs
+++ TestBigData/TestAPI.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BigData;
 
 namespace TestBigData
 {
     class TestAPI : Test
     {
-        public void TestAPIAccess()
+        public void TestPublicationCreation()
         {
-           //OCLCWrapper wrapper = new OCLCWrapper(someurl);
+            // This url points to the RSS feed for the list
+            var listURL = "https://bucknell.worldcat.org/profiles/danieleshleman/lists/3234701/rss";
+            OCLCWrapper wrapper = new OCLCWrapper(listURL);
+            //System.Console.WriteLine(wrapper.getXML());
+            List<Publication> list = wrapper.createPublications();
+            if (!list.Any())
+            {
+                Fail("No valid publications found");
+            }
+            else
+            {
+                Pass(); //#All the data
+            }
         }
     }
 }
