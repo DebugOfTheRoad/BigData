f8528f61d9cfe5fa480b62ee190556006d224f48
Daniel Eshleman <dae013@bucknell.edu>
2014-01-26 10:51:47 -0500

    Add OCLC wrapper, incomplete

---
+++ BigData/OCLC/OCLCWrapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using urllib;
+using Publication;
+
+public class OCLCWrapper
+{
+    String feed;
+	public OCLCWrapper(string rss)
+	{
+        feed = urlopen(rss);
+
+	}
+
+    List<Publication> PublicationsByISBN(String isbn)
+    {
+
+    }
+}
---
+++ BigData/OCLC/Publication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Publication    
+{
+    public List<String> ISBNs;
+    public List<String> Authors;
+    public List<Publication> RelatedPublications;
+    public DateTime dateAdded;
+    // public Image coverImage;
+
+	public Publication(String isbn, String title, List<String> author) 
+	{
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+	}
+}
