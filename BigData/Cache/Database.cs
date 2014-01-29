ef2d34c7b7998f56c07ebc4feec103bc399ff69d
Chris Cook <oxherder@msn.com>
2014-01-29 16:53:11 -0500

    merge wat

--- BigData/Cache/Database.cs
+++ BigData/Cache/Database.cs
@@ -5,32 +5,63 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
 
-namespace BigData.Cache {
+namespace BigData {
     public class Database {
         private SQLiteConnection sql_con;
+<<<<<<< HEAD
         private SQLiteCommand sql_cmd;
         //private SQLiteDataAdapter db;
+=======
+        private string rss_feed;
+>>>>>>> Added database test
         
-        public Database() {
-            sql_con = new SQLiteConnection("Data Source = publications.db, Version = 3, New = false, Compress = true");
+        public Database(string feed) {
+            SQLiteConnection.CreateFile("publications.db");
+            sql_con = new SQLiteConnection("Data Source = publications.db; Version = 3; New = false; Compress = true");
+            rss_feed = feed;
         }
         
-        private void create_db() {
-            string create_table = "CREATE TABLE Publications(title char(64), link char(64), Description char(128))";
-            this.query(create_table);
+        public void create_db() {
+            string create_table = "CREATE TABLE Publications(title char(64))";
+            this.sql_command(create_table);
             this.update_db();
         }
         
-        private void update_db() {
-            sql_con.Open();
+        public void update_db() {
+            OCLCWrapper wrapper = new OCLCWrapper(this.rss_feed);
+            List<Publication> pub_list = wrapper.createPublications();
+
+            string insert_query;
+            for (int i = 0; i < pub_list.Count; i++) {
+                insert_query =  "INSERT INTO Publications VALUES (\"" + pub_list[i].title + "\");";
+                this.sql_command(insert_query);
+            }
         }
 
-        private void query(string query) {
+        private void sql_command(string cmd) {
             sql_con.Open();
-            sql_cmd = sql_con.CreateCommand();
-            sql_cmd.CommandText = query;
+            SQLiteCommand sql_cmd = new SQLiteCommand(cmd, sql_con);
             sql_cmd.ExecuteNonQuery();
             sql_con.Close();
+        }
+
+        private SQLiteDataReader sql_query(string query) {
+            sql_con.Open();
+            SQLiteCommand sql_cmd = new SQLiteCommand(query, sql_con);
+            SQLiteDataReader reader = sql_cmd.ExecuteReader();
+            //sql_con.Close();
+            return reader;
+        }
+
+        public string print_all() {
+            string s = "";
+            string query = "SELECT * FROM Publications;";
+            SQLiteDataReader reader = this.sql_query(query);
+            while (reader.Read()) {
+                s = s + "Entry: " + reader["title"] + "\n";
+            }
+            sql_con.Close();
+            return s; 
         }
     }
 }
---
+++ TestBigData/TestDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BigData;
+
+namespace TestBigData {
+    class TestDB : Test{
+        public void TestDatabaseCreation() {
+            var listURL = "https://bucknell.worldcat.org/profiles/danieleshleman/lists/3234701/rss";
+            Database db = new Database(listURL);
+            db.create_db();
+            System.Console.WriteLine(db.print_all());
+            System.Console.WriteLine("bitches");
+            Pass(); // Look at output Muhahah
+        }
+    }
+}
