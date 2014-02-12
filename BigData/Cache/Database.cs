b97ae9b5548ce86e7ac75038adecbd0c4ce7c6f6
Chris Cook <oxherder@msn.com>
2014-02-12 13:39:47 -0500

    Added more queries and added testdb back to project

--- BigData/Cache/Database.cs
+++ BigData/Cache/Database.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,7 +21,12 @@ namespace BigData {
         }
         
         public void create_db() {
-            string create_table = "CREATE TABLE Publications(title TEXT)";
+            string create_table = "CREATE TABLE Publications(" +
+                                  "isbn TEXT PRIMARY KEY, " +
+                                  "title TEXT, " +
+                                  "date_added INT, " +
+                                  "cover BLOB" +
+                                  ")";
             this.sql_command(create_table);
             this.update_db();
         }
@@ -31,7 +37,13 @@ namespace BigData {
 
             string insert_query;
             for (int i = 0; i < pub_list.Count; i++) {
-                insert_query =  "INSERT INTO Publications VALUES (\"" + pub_list[i].title + "\");";
+                insert_query =  "INSERT INTO Publications VALUES (" + 
+                                "\"" + i + "\", " + 
+                                "\"" + pub_list[i].title + "\", " +
+                                "1234" + ", " +
+                                "\"" + "deadbeef" + "\"" +
+                                ");";
+                Console.WriteLine(insert_query);
                 this.sql_command(insert_query);
             }
         }
@@ -52,9 +64,35 @@ namespace BigData {
             string query = "SELECT * FROM Publications;";
             SQLiteDataReader reader = this.sql_query(query);
             while (reader.Read()) {
-                s = s + "Entry: " + reader["title"] + "\n";
+                s = s + "Entry: " + reader["isbn"] + 
+                        "\n\tTitle:\t" + reader["title"] +
+                        "\tDate:\t" + reader["date_added"] + "\n";
             }
             return s;
+        }
+
+        public ArrayList get_new_books(int date) {
+            ArrayList book_list = new ArrayList();
+            string query = "SELECT isbn, cover FROM Publications WHERE date_added >= " + date + ";";
+            SQLiteDataReader reader = this.sql_query(query);
+            while (reader.Read()) {
+                var tup = Tuple.Create(reader["isbn"], reader["cover"]);
+                book_list.Add(tup);
+            }
+            return book_list;
+        }
+
+        public Publication get_isbn(string isbn) {
+            Publication book = new Publication("", "", "");
+            string query = "SELECT * FROM Publications WHERE isbn = \"" + isbn + "\";";
+            SQLiteDataReader reader = this.sql_query(query);
+            while (reader.Read()) {
+                // book.ISBN = (string) reader["title"];
+                book.title = (string) reader["title"];
+                // book.dateAdded = (int) reader["date_added"];
+                // book.coverImage = (Image) reader["cover"];
+            }
+            return book;
         }
 
         public void close() {
