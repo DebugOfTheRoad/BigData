75da59a3d7ff2bd17fce107ee19d5ebb92e392f9
Chris Cook <oxherder@msn.com>
2014-03-05 16:29:12 -0500

    yeeemaaill

---
+++ BigData/Emailer/Emailer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace BigData.Emailer {
+    public class Emailer {
+        public static void send_email(string rec, string sub, string msg) {
+            MailMessage email = new MailMessage();
+            email.To.Add(rec);
+            email.Subject = sub;
+            email.From = new MailAddress("cec030@bucknell.edu");
+            email.Body = msg;
+            SmtpClient smtp = new SmtpClient("bucknell.edu.s10a1.psmtp.com", 25);
+            //smtp.EnableSsl = true;
+            smtp.Timeout = 10000;
+            
+            // Send the email
+            try {
+                smtp.Send(email);
+            } catch (Exception e) {
+                Console.WriteLine("Error: " + e);
+            } 
+        }
+    }
+}
