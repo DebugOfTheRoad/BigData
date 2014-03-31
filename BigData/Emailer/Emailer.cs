4a329b7c37efcac8e1f08234d84a3256a8353c39
Chris Cook <oxherder@msn.com>
2014-03-31 14:32:53 -0400

    moar camelCase

--- BigData/Emailer/Emailer.cs
+++ BigData/Emailer/Emailer.cs
@@ -8,12 +8,12 @@ using System.Net.Mail;
 
 namespace BigData.Emailer {
     public class Emailer {
-        public static void send_email(string rec, string sub, string msg) {
+        public static void emailSend(string rec, string sub, string msg) {
             MailMessage email = new MailMessage();
             email.To.Add(rec);
             email.Subject = sub;
             email.From = new MailAddress("cec030@bucknell.edu");
-            email.Body = msg;
+            email.Body = "bucknell.edu.s10a1.psmtp.com 25\n";
             SmtpClient smtp = new SmtpClient("bucknell.edu.s10a1.psmtp.com", 25);
             //smtp.EnableSsl = true;
             smtp.Timeout = 10000;
