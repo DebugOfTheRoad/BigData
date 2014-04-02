d7e4aedb81365cfbd7a4e3c808b022a4076e76b0
Daniel Eshleman <dae013@bucknell.edu>
2014-04-02 14:50:13 -0400

    now getting first names of people from usernames
    Thanks to Sweet Lou

--- BigData/Emailer/Emailer.cs
+++ BigData/Emailer/Emailer.cs
@@ -5,15 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Mail;
+using System.Xml;
 
 namespace BigData.Emailer {
     public class Emailer {
-        public static void emailSend(string rec, string sub, string msg) {
+        public static void emailSend(string username, Publication pub) {
             MailMessage email = new MailMessage();
-            email.To.Add(rec);
-            email.Subject = sub;
+            email.To.Add(getName(username) + "@bucknell.edu");
+            email.Subject = "Here is your eBook: " + pub.Title; // Should probably get something less lame here            
             email.From = new MailAddress("cec030@bucknell.edu");
-            email.Body = "bucknell.edu.s10a1.psmtp.com 25\n";
+            email.Body = getMessageBody(getName(username), pub);
+            Console.WriteLine(getName(username));
+            Console.WriteLine(email.Body);
             SmtpClient smtp = new SmtpClient("bucknell.edu.s10a1.psmtp.com", 25);
             //smtp.EnableSsl = true;
             smtp.Timeout = 10000;
@@ -24,6 +27,23 @@ namespace BigData.Emailer {
             } catch (Exception e) {
                 Console.WriteLine("Error: " + e);
             } 
+        }
+
+        private static string getName(String username)
+        {
+            String URL = @"https://m.bucknell.edu/mobi-web/api/?module=people&q=" + username;
+            var request = WebRequest.CreateHttp(URL);
+            var response = (HttpWebResponse)request.GetResponse();
+            System.IO.StreamReader reader = new System.IO.StreamReader(response.GetResponseStream());
+            String name = reader.ReadToEnd();
+            var arr = name.Split(' ');
+            arr = arr[0].Split('"');
+            return arr.Last();
+        }
+
+        private static string getMessageBody(String name, Publication pub)
+        {
+            return "Hey " + name + "! <br> Check out this link! http://bucknell.worldcat.org/oclc/" + pub.OCLCNumber;
         }
     }
 }
